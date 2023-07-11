1d2ee88f4e18ce420f5ec6d05cbb60947a4f9b33
chrisATYF <36751474+chrisATYF@users.noreply.github.com>
2023-07-10 22:37:17 -0500

    Added footer

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,17 @@ import Nav from './components/Nav.vue';
 <template>
   <Nav></Nav>
   <RouterView />
+  <div class="btmContent">
+    <p>&copy 2023 - Norsemen Hauling</p>
+  </div>
 </template>
+
+<style scoped>
+.btmContent {
+  bottom: 0;
+  width: 100%;
+  padding: 5px;
+  margin: 0;
+  text-align: center;
+}
+</style>
