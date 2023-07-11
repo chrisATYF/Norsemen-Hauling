b08a5d5ba0e0536ea0e2a65b8e94fec9539c0832
chrisATYF <36751474+chrisATYF@users.noreply.github.com>
2023-07-11 14:28:26 -0500

    Added photos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,6 +11,10 @@ import Intro from '../components/Introduction.vue';
         aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
         aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3"
+        aria-label="Slide 4"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4"
+        aria-label="Slide 5"></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
@@ -21,6 +25,12 @@ import Intro from '../components/Introduction.vue';
       </div>
       <div class="carousel-item">
         <img src="../../images/Trailer.jpg" id="CarImg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="../../images/NoahWork.jpg" id="CarImg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="../../images/Shieldmaidens.jpg" id="CarImg" class="d-block w-100" alt="...">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
